8d8780ef62c3bfba996b306762b30839158dd7bc
Bin Wei <bin.wei@intel.com>
2012-11-06 15:55:32 -0800

    Update CentOS6.3_HHVM/buildhhvm/3_unziphhvm.sh

--- CentOS6.3_HHVM/buildhhvm/3_unziphhvm.sh
+++ CentOS6.3_HHVM/buildhhvm/3_unziphhvm.sh
@@ -10,6 +10,6 @@ BUILD_HOME=/home/dev
 #3. unpack the hiphop-php
 cd $BUILD_HOME
 unzip $BUILD_HOME/centos/hhvmsrc/*.zip
-mv facebook-hiphop*  hiphop-php
+mv hiphop*  hiphop-php
 cd $BUILD_HOME/hiphop-php
 source /home/dev/centos/buildhhvm/setenv.sh
