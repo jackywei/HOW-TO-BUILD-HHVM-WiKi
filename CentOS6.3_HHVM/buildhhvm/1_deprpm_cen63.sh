b0fdce71d59a01590b7049118108259715109af9
jackywei <bin.wei@intel.com>
2013-01-15 13:59:01 +0800

    fixed modify CentOS-Media.repo file

--- CentOS6.3_HHVM/buildhhvm/1_deprpm_cen63.sh
+++ CentOS6.3_HHVM/buildhhvm/1_deprpm_cen63.sh
@@ -12,6 +12,6 @@ mkdir /media/cdrom2
 mount -t iso9660 -o loop $BUILD_HOME/centos/centosimage/CentOS-6.3-x86_64-bin-DVD1.iso /media/cdrom1
 mount -t iso9660 -o loop $BUILD_HOME/centos/centosimage/CentOS-6.3-x86_64-bin-DVD2.iso /media/cdrom2
 cp /etc/yum.repos.d/CentOS-Media.repo /etc/yum.repos.d/CentOS-Media.repo.back
-sed -e 's/cdrom/cdrom1/g' -e 's/CentOS/cdrom2/g' /etc/yum.repos.d/CentOS-Media.repo.back > /etc/yum.repos.d/CentOS-Media.repo
+sed -e 's/cdrom/cdrom1/g' -e 's/CentOS\//cdrom2\//g' /etc/yum.repos.d/CentOS-Media.repo.back > /etc/yum.repos.d/CentOS-Media.repo
 yum --disablerepo=\* --enablerepo=c6-media install git svn cpp make autoconf automake libtool patch memcached cmake wget boost-devel mysql-devel pcre-devel gd-devel libxml2-devel expat-devel libicu-devel bzip2-devel oniguruma-devel openldap-devel readline-devel libc-client-devel libcap-devel binutils-devel pam-devel elfutils-libelf-devel
 
