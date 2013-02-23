b989a08494e1ebfe86f4f30d22e1359738ad3478
Bin Wei <bin.wei@intel.com>
2013-02-23 15:37:30 +0800

    Update CentOS5.2_HHVM/buildhhvm/5_libevent.sh
    directory name is changed in the latest hhvm code based, from src to hphp. this will cause libevent and libcurl could not be patched.

--- CentOS5.2_HHVM/buildhhvm/5_libevent.sh
+++ CentOS5.2_HHVM/buildhhvm/5_libevent.sh
@@ -8,7 +8,7 @@ cd $BUILD_HOME
 #5. Install libevent
 unzip $BUILD_HOME/centos/dependsapp/libevent-libevent-release-1.4.14b-stable-0-geb8122f.zip
 cd libevent-libevent-deb8b9a/
-cat ../hiphop-php/src/third_party/libevent-1.4.14.fb-changes.diff | patch -p1
+cat ../hiphop-php/hphp/third_party/libevent-1.4.14.fb-changes.diff | patch -p1
 ./autogen.sh
 ./configure --prefix=$CMAKE_PREFIX_PATH
 make
